a2ae901d25ab2385097bbb7f9c8a8c646355f6d7
Eddie <eddiezhou1996@gmail.com>
2018-06-13 18:11:22 -0700

    Update compare.py

--- compare/compare.py
+++ compare/compare.py
@@ -50,8 +50,8 @@ class Compare:
         if os.path.splitext(attachment["filename"])[1] != ".ics":
             return await self.bot.say("Please upload a valid .ics file")
 
-        await self.bot.delete_message(message)
         async with aiohttp.get(attachment["url"]) as resp:
+            await self.bot.delete_message(message)
             if resp.status != 200:
                 return await self.bot.say("An unknown issue occurred, try again later!")
             calendar = await resp.text()
