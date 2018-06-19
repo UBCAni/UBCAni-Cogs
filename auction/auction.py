8a2f45ff8371899b2dfa82cbb3f2c5f8c8ad7807
Eddie Zhou <eddiezhou1996@gmail.com>
2018-06-18 23:36:48 -0700

    Add await

--- auction/auction.py
+++ auction/auction.py
@@ -34,9 +34,9 @@ class Auction:
 
     @auction.command(pass_context=True, no_pm=True)
     async def bid(self, ctx, amount : int, user : discord.Member = None):
-        self._bid(ctx, amount, user)
+        await self._bid(ctx, amount, user)
 
-    def _bid(self, ctx, amount, user):
+    async def _bid(self, ctx, amount, user):
         """Place a bid on another user"""
 
         author = ctx.message.author
@@ -197,7 +197,7 @@ class Auction:
 
         delta = raise_from - sum(self.data[server.id][user.id].values()) + amount
 
-        self._bid(ctx, delta, user)
+        await self._bid(ctx, delta, user)
 
     def _reset(self, server, user):
         """Resets all of the user's bids on others to 0, and returns a dictionary where the keys are the original users and the values are the removed amounts"""
