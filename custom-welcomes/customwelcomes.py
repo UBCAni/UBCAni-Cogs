e1dc1f88ff8bef46cb5ccd06336c053e9b8a046c
Jacob <jacoblacsamana0@gmail.com>
2022-07-08 04:09:04 -0700

    created custom welcomes cog

---
+++ custom-welcomes/customwelcomes.py
@@ -0,0 +1,13 @@
+from redbot.core import commands
+
+class CustomWelcomes(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
