76ec1d13535e69c7995bd7c3a030a23c257a71e4
Jacob <jacoblacsamana0@gmail.com>
2022-08-14 09:35:05 -0700

    organised toggle seting commands into a group

--- customwelcomes/customwelcomes.py
+++ customwelcomes/customwelcomes.py
@@ -34,14 +34,19 @@ class CustomWelcomes(commands.Cog):
         await channel.send("test message")
 
     ### UTLITY COMMANDS ###
-    @commands.command()
+    @commands.group(aliases=["gsettings"])
+    async def greetsettings(self, ctx: commands.Context):
+        """Base command for configuring the customised welcome."""
+        pass
+
+    @greetsettings.group(name="setch")
     async def setwelcomech(self, ctx):
         """Call this in the channel where you want to display welcomes"""
         # Your code will go here
         await self.config.guild(ctx.author.guild).welcome_msg_channel.set(ctx.channel.id)
         await ctx.send("New welcome channel is: " + ctx.channel.name)
 
-    @commands.command()
+    @greetsettings.group(name="getstatus")
     async def getwelcomestatus(self, ctx):
         """Call this in the channel where you want to display welcomes"""
         # Your code will go here
@@ -50,7 +55,7 @@ class CustomWelcomes(commands.Cog):
         await ctx.send("Sending custom message: " + str(await self.config.guild(ctx.author.guild).get_attr("toggle_msg")()))
         await ctx.send("Sending custom image: "+ str(await self.config.guild(ctx.author.guild).get_attr("toggle_img")()))
 
-    @commands.command()
+    @greetsettings.group(name="togglemsg")
     async def togglewelmsg(self, ctx):
         """Call this to toggle welcome message on and off"""
         value = await self.config.guild(ctx.author.guild).get_attr("toggle_msg")()
@@ -63,7 +68,7 @@ class CustomWelcomes(commands.Cog):
 
         await ctx.send("Sending custom message set to " + str(await self.config.guild(ctx.author.guild).get_attr("toggle_msg")()))
 
-    @commands.command()
+    @greetsettings.group(name="toggleimg")
     async def togglewelimg(self, ctx):
         """Call this to toggle welcome image on and off"""
         value = await self.config.guild(ctx.author.guild).get_attr("toggle_img")()
@@ -75,6 +80,23 @@ class CustomWelcomes(commands.Cog):
         await self.config.guild(ctx.author.guild).toggle_img.set(value)
 
         await ctx.send("Sending custom image set to "+ str(await self.config.guild(ctx.author.guild).get_attr("toggle_img")()))
+
+    ### UTLITY COMMANDS ###
+
+
+
+    ### CUSTOM WELCOME PICTURE GENERATION ###
+    def get_welcome_img(self):
+        pass
+
+
+
+    ### CUSTOM WELCOME MESSAGE GENERATION ###
+    def get_welcome_msg(self):
+        pass
+    
+
+
 
 #   @commands.command()
 #   async def mycom(self, ctx):
